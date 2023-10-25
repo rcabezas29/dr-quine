4cdfecaadfb2effc2b6d6944c69f5953d562a779
Raúl Cabezas González <raul.29cg@gmail.com>
2023-10-25 11:00:06 +0200

    Added comments on Collen ASM

--- ASM/Collen.s
+++ ASM/Collen.s
@@ -1,11 +1,13 @@
+; Comment out of the program
 section .data
-    msg db "section .data%1$c    msg db %2$c%3$s%2$c%1$c    extern _printf%1$c%1$csection .text%1$c    global _main%1$c%1$c_main:%1$c    push rbp%1$c%1$c    mov rdi, msg%1$c    mov	rsi, 10%1$c    mov	rdx, 34%1$c    mov	rax, 0%1$c    call _printf%1$c%1$c%1$c    pop	rbp%1$c    mov	rax, 0%1$c    ret%1$c"
+    msg db "; Comment out of the program%1$csection .data%1$c    msg db %2$c%3$s%2$c%1$c    extern _printf%1$c%1$csection .text%1$c    global _main%1$c%1$c_main:%1$c    ; Comment inside the main function%1$c    push rbp%1$c%1$c    mov rdi, msg%1$c    mov	rsi, 10%1$c    mov	rdx, 34%1$c    mov	rax, 0%1$c    call _printf%1$c%1$c%1$c    pop	rbp%1$c    mov	rax, 0%1$c    ret%1$c"
     extern _printf
 
 section .text
     global _main
 
 _main:
+    ; Comment inside the main function
     push rbp
 
     mov rdi, msg
