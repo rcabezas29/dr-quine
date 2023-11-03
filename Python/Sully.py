49c8494a7e9e207ea3254f9c16a89dcb995c613c
Raúl Cabezas González <raul.29cg@gmail.com>
2023-11-03 16:24:47 +0100

    Fixed Sully Python

--- Python/Sully.py
+++ Python/Sully.py
@@ -1,8 +1,8 @@
 import os
-quine = 'import os\nquine = %r\nn = %d\nif (n >= 0):\n    with open("Sully_%%d.py" %% n, "w") as f:\n        f.write(quine %% (quine, n - 1))\n        f.close()\n    os.system("python3 Sully_%%d.py" %% (n - 1))\n'
+quine = 'import os\nquine = %r\nn = %d\nif (n >= 0):\n    with open("Sully_%%d.py" %% n, "w") as f:\n        f.write(quine %% (quine, n - 1))\n        f.close()\n    os.system("python3 Sully_%%d.py" %% n)\n'
 n = 5
 if (n >= 0):
     with open("Sully_%d.py" % n, "w") as f:
         f.write(quine % (quine, n - 1))
         f.close()
-    os.system("python3 Sully_%d.py" % (n - 1))
+    os.system("python3 Sully_%d.py" % n)
