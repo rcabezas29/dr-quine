b0aa2758417cf76cc1ae1fe00b292cefa10602f6
Raúl Cabezas González <raul.29cg@gmail.com>
2023-11-09 19:37:26 +0100

    Done bonus part

--- Python/Sully.py
+++ Python/Sully.py
@@ -1,8 +1,10 @@
 import os
-quine = 'import os\nquine = %r\nn = %d\nif (n >= 0):\n    with open("Sully_%%d.py" %% n, "w") as f:\n        f.write(quine %% (quine, n - 1))\n        f.close()\n    os.system("python3 Sully_%%d.py" %% n)\n'
+import sys
+quine = 'import os\nimport sys\nquine = %r\nn = %d\nif (n > 0):\n    if len(sys.argv) > 1: n -= 1\n    with open("Sully_%%d.py" %% n, "w") as f:\n        f.write(quine %% (quine, n))\n        f.close()\n    os.system("python3 Sully_%%d.py X" %% n)\n'
 n = 5
-if (n >= 0):
+if (n > 0):
+    if len(sys.argv) > 1: n -= 1
     with open("Sully_%d.py" % n, "w") as f:
-        f.write(quine % (quine, n - 1))
+        f.write(quine % (quine, n))
         f.close()
-    os.system("python3 Sully_%d.py" % n)
+    os.system("python3 Sully_%d.py X" % n)
