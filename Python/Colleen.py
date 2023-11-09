d9d6cf0d73439d534737d5b0279f5008ba9afce8
Raúl Cabezas González <raul.29cg@gmail.com>
2023-11-09 19:38:47 +0100

    Fixed Colleen Python

--- Python/Colleen.py
+++ Python/Colleen.py
@@ -1,6 +1,6 @@
 # Comment outside function
 def quine():
-    s = '# Comment outside function\ndef quine():\n    s = %r\n    print(s %% s)\n\ndef main():\n    # Comment inside main function\n    quine()\n\nmain()\n'
+    s = '# Comment outside function\ndef quine():\n    s = %r\n    print(s %% s)\n\ndef main():\n    # Comment inside main function\n    quine()\n\nmain()'
     print(s % s)
 
 def main():
